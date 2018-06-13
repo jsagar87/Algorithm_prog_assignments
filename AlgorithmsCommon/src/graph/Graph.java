2265917048e0bfaf26897e5bbd8d976109defe48
sagar <jsagar87@gmail.com>
2018-06-13 09:04:36 +0530

    Graph helper methods

--- AlgorithmsCommon/src/graph/Graph.java
+++ AlgorithmsCommon/src/graph/Graph.java
@@ -1,7 +1,12 @@
 package graph;
 
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
 
+/**
+ * Api to cover representation of the graph, and simple
+ * set of methods for clients to process the graph.
+ */
 public class Graph {
 
     /**
@@ -14,8 +19,63 @@ public class Graph {
      */
     Graph(In in){}
 
+    /**
+     * Add an edge v-w
+     *
+     * @param v
+     * @param w
+     */
     void addEdge(int v,int w){}
 
+    /**
+     * Vertices adjacent to v
+     *
+     * @param v
+     * @return
+     */
     Iterable<Integer> adj(int v){return null;}
 
+    /**
+     * Number of vertices
+     *
+     * @return
+     */
+    int V(){
+        return 0;
+    }
+
+    /**
+     * Number of edges
+     *
+     * @return
+     */
+    int E(){
+        return 0;
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    /*
+     Client program that prints out every edge in graph
+     */
+    public static void main(String[] args) {
+
+
+        In in  =  new In(args[0]);
+        Graph G = new Graph(in);               // Create graph from input stream
+
+        // v = 0 to v-1
+        for (int v = 0; v<G.V(); v++){
+            for (int w  : G.adj(v)){
+                StdOut.println(v + " - " + w); // Print out edge (twice)
+            }
+        }
+    }
 }
--- AlgorithmsCommon/src/graph/HelperFunctions.java
+++ AlgorithmsCommon/src/graph/HelperFunctions.java
@@ -1,8 +1,12 @@
 package graph;
 
+/**
+ * Simple and typical graph processing code that uses the API
+ */
 public class HelperFunctions {
     /**
      * Compute the degree of v
+     *
      * @param G
      * @param v
      * @return
@@ -14,10 +18,42 @@ public class HelperFunctions {
         return degree;
     }
 
-    public static int maxDegree(Graph G, int v)
+    /**
+     * Compute maximum degree
+     *
+     * @param G
+     * @return
+     */
+    public static int maxDegree(Graph G)
     {
         int max = 0;
-        
+        for (int v = 0; v < G.V(); v++)
+            if (degree(G,v) > max)
+                max = degree(G,v);
         return max;
+    }
+
+    /**
+     * Compute average degree
+     * @param G
+     * @return
+     */
+    public static double averageDegree(Graph G)
+    {
+        return 2.0*G.E()/G.V();
+    }
+
+    /**
+     * Count self-loops
+     * @param G
+     * @return
+     */
+    public static int numberOfSelfLoops(Graph G)
+    {
+        int count = 0;
+        for (int v = 0; v<G.V(); v++ )
+            for(int w : G.adj(v))
+                if (v == w) count++;
+        return count/2; // each edge counted twice
     }
 }
